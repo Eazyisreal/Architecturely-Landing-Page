675f33debf339f159481a9cb6b2386a5c4f61f71
Eazyisreal <eazyisreal192@gmail.com>
2023-05-17 22:34:45 +0100

    [Chore]: Completed Testimonial  section across all screen sizes

--- Architecturely/src/components/Impact.jsx
+++ Architecturely/src/components/Impact.jsx
@@ -4,7 +4,7 @@ import arrow from "../assets/images/arrow.svg";
 export default function Impact() {
   return (
     <>
-      <section className="bg-white pt-20 px-8 ">
+      <section className="bg-white wrapper pt-20 px-8 ">
         <div>
           <div className="max-md:pl-[3.5rem]">
             <h2 className=" text-black pb-12 font-bold text-4xl max-md:text-[3.5rem]  max-md:leading-[1.3] leading-[1.3] font-onest ">
--- Architecturely/src/components/Services.jsx
+++ Architecturely/src/components/Services.jsx
@@ -7,47 +7,49 @@ export default function Services() {
     return (
         <>
             <div className="bg-black flex pt-20 px-8 flex-col">
-                <div className="max-md:flex  max-md:gap-[20rem] ">
-                    <h2 className="text-white whitespace-nowrap font-medium text-4xl pb-12 font-onest  ">
-                        Our services
-                    </h2>
-                    <p className="text-[#A5A5A5] max-lg:pl-[20rem] max-lg:pr-[12rem]  text-base leading-[2]  pb-8 font-normal font-onest ">
-                        We take pride in our commitment to quality, creativity, and client
-                        satisfaction. Our goal is to provide you with a service that not only
-                        meets your expectations but exceeds them. Contact us today to learn
-                        more about how we can help bring your architectural dreams to life.
-                    </p>
-                </div>
-
-                <div className="flex pb-8 snap-x snap-mandatory gap-4 flex-row  w-full">
-                    <div className="flex flex-col w-full">
-                        <img
-                            className="w-full snap-start "
-                            src={architecture}
-                            alt="Architectural Design Image"
-                        />
-                        <p className="text-white font-normal pt-4 text-xl font-onest">Architectural Design</p>
+                <div className="wrapper">
+                    <div className="max-md:flex  max-md:gap-[20rem] ">
+                        <h2 className="text-white whitespace-nowrap font-medium text-4xl pb-12 font-onest  ">
+                            Our services
+                        </h2>
+                        <p className="text-[#A5A5A5] max-lg:pl-[20rem] max-lg:pr-[12rem]  text-base leading-[2]  pb-8 font-normal font-onest ">
+                            We take pride in our commitment to quality, creativity, and client
+                            satisfaction. Our goal is to provide you with a service that not only
+                            meets your expectations but exceeds them. Contact us today to learn
+                            more about how we can help bring your architectural dreams to life.
+                        </p>
                     </div>
 
-                    <div className="flex flex-col w-full">
-                        <img
-                            className="w-full snap-start "
-                            src={interior}
-                            alt="Interior Design Image"
-                        />
-                        <p className="text-white font-normal pt-4 text-xl font-onest">Interior Design</p>
+                    <div className="flex pb-8 snap-x snap-mandatory gap-4 flex-row  w-full">
+                        <div className="flex flex-col w-full">
+                            <img
+                                className="w-full snap-start "
+                                src={architecture}
+                                alt="Architectural Design Image"
+                            />
+                            <p className="text-white font-normal pt-4 text-xl font-onest">Architectural Design</p>
+                        </div>
+
+                        <div className="flex flex-col w-full">
+                            <img
+                                className="w-full snap-start "
+                                src={interior}
+                                alt="Interior Design Image"
+                            />
+                            <p className="text-white font-normal pt-4 text-xl font-onest">Interior Design</p>
+                        </div>
+
+                        <div className="flex flex-col w-full">
+                            <img
+                                className="w-full snap-start "
+                                src={decoration}
+                                alt="Architectural Design Image"
+                            />
+                            <p className="text-white font-normal pt-4 text-xl font-onest">Decoration</p>
+                        </div>
+
+
                     </div>
-
-                    <div className="flex flex-col w-full">
-                        <img
-                            className="w-full snap-start "
-                            src={decoration}
-                            alt="Architectural Design Image"
-                        />
-                        <p className="text-white font-normal pt-4 text-xl font-onest">Decoration</p>
-                    </div>
-
-
                 </div>
             </div>
         </>
